18e332681493bb4038e6e386a3c285bfa34595f4
Simon <geigersimon36@gmail.com>
2020-06-21 14:44:53 +0200

    kommentare server.ts

--- Aufgabe08/server.ts
+++ Aufgabe08/server.ts
@@ -1,29 +1,38 @@
-  
 import * as Http from "http";
 
 export namespace A08Server {
-  console.log("Starting server");
-  let port: number = Number(process.env.PORT);
-  if (!port)
-    port = 8100;
+    //Konsolenausgabe Serverstart
+    console.log("Starting server");
+    //port Vairable erstellen und port zuweisen
+    let port: number = Number(process.env.PORT);
+    //port abchecken und zuweisen
+    if (!port)
+        port = 8100;
 
-  let server: Http.Server = Http.createServer();
-  server.addListener("request", handleRequest);
-  server.addListener("listening", handleListen);
-  server.listen(port);
+    //Server variable erstellen
+    let server: Http.Server = Http.createServer();
+    //handler hinzufügen
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    //server soll auf port hören und schauen ob es dort anfragen gibt.
+    server.listen(port);
 
-  function handleListen(): void {
-    console.log("Listening");
-  }
+    //Konsolenausgabe
+    function handleListen(): void {
+        console.log("Listening");
+    }
 
-  function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-    console.log("I hear voices!");
 
-    _response.setHeader("content-type", "text/html; charset=utf-8");
-    _response.setHeader("Access-Control-Allow-Origin", "*");
-
-    _response.write(_request.url);
-
-    _response.end();
-  }
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        //Konsolenausgabe
+        console.log("I hear voices!");
+        //Parameter des Headers der Response festlegen
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        //Konoslenausgabe URL
+        _response.write(_request.url);
+        console.log(_request.url);
+        //response ende
+        _response.end();
+    }
 }
