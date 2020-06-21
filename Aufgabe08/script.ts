143b1fc197771e795722ff8ed88413f788e5f31e
Simon <geigersimon36@gmail.com>
2020-06-21 14:20:38 +0200

    aufgabe 8 start

---
+++ Aufgabe08/script.ts
@@ -0,0 +1,17 @@
+namespace Aufgabe08 {
+    let formData: FormData;
+    let buttonAction: HTMLButtonElement = <HTMLButtonElement> document.getElementById("performAction");
+    buttonAction.addEventListener("click", handleClick);
+
+    async function handleClick(): Promise<void> {
+        formData = new FormData(document.forms[0]);
+        let serverURL: string = "https://gissose2020simon.herokuapp.com/";
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        serverURL += "?" + query.toString();
+
+        let response: Response = await fetch(serverURL);
+        let responseText: string = await response.text();
+        console.log(responseText);
+    }
+
+}
